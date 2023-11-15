fff122bf005a82522012d9ccc6f6afe73e35d2ff
rachaelbabister <>
2023-11-15 00:20:49 +0000

    Add functions to class Tomahawk to launch game play

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import pyfiglet
 import time
 import os
-import random
+from random import randrange
 
 
 # CONSOLE CLEARING
@@ -34,7 +34,7 @@ class TargetBoards:
         for i in range(self.target_hits):
             hit = self.random_hit()
             self.hit_positions.append(hit)
-            self.add_to_state(hit, "0")
+            #self.add_to_state(hit, "0")
         self.direct_hit = []
         self.missed_hit = []
 
@@ -48,22 +48,18 @@ class TargetBoards:
                 return " "
             return cell    
 
-        user_target = f"\n{user_name}'s Target Board:\n"
-        computer_target = "\nComputer's Target Board:\n"
-
-        for i, (user_row, computer_row) in enumerate(zip(self.target_size, self.target_size)):
+        # Create Target Grid
+        target = ""
+        for i, row in enumerate(self.target_size):
             if i == 0:
-                user_target += "+" + "---+" * (self.size - 1) + "\n"
-                computer_target += "+" + "---+" * (self.size - 1) + "\n"
-
-            user_target += "".join([f"| {hit_hiddenn(cell)}" for cell in user_row]) + "|\n"
-            computer_target += "".join([f"| {hit_hidden(cell)}" for cell in computer_row]) + "|\n"
-
+                target += "+" + "---+" * (self.size - 1) + "\n"
+                
+            target += "".join([f"| {hit_hidden(cell)}" for cell in user_row]) + "|\n"
+            
             if i == len(self.target_size) - 1:
-                user_target += "+" + "---+" * (self.size - 1) + "\n"
-                computer_target += "+" + "---+" * (self.size - 1) + "\n"    
-   
-        print(user_target + computer_target)
+                target += "+" + "---+" * (self.size - 1) + "\n"
+                
+        print(target)
 
     def random_hit(self):
         """
@@ -84,64 +80,68 @@ class TargetBoards:
 
 
 # GAME LOGO AND WELCOME MESSAGE
-def game_logo():
+class TomaHawkGame:
     """
-    Will display the game name and welcome message in ASCII Art.
-    After a pause, the user will be asked to enter their name.
+    Functions to launch the game play.
     """
-    logo = pyfiglet.figlet_format("Tomahawk Tactics") 
-    welcome = pyfiglet.figlet_format("Welcome to Tomahawk Tactics", font = "digital") 
-    print(logo, welcome, "\nby Rachael Babister\n")
-    print("\nGame Loading...\n")
-    time.sleep(2)
-    return run_game()
+    def throw_axe(self, target):
 
 
-# ENTER NAME AND START GAME
-def run_game():
-    """
-    User enters their name in order for the game rules to appear.
-    Once validated, the user can then specify y/n to play the game.
-    """
-    while True:
-        global user_name 
-        user_name = input("Enter your name: ")
-        if user_name == "":
-            print("You need to enter your name in order to play")
-        else:
-            break
 
-    clear()
-    print(pyfiglet.figlet_format("Welcome to Tomahawk Tactics", font = "digital"))
-    print()
-    print(f"Hello {user_name}! \n")
-    print("""
+    def run_game(self):
+        """
+        Will display the game name and welcome message in ASCII Art.
+        After a pause, the user will be asked to enter their name.
+        """
+        logo = pyfiglet.figlet_format("Tomahawk Tactics") 
+        welcome = pyfiglet.figlet_format("Welcome to Tomahawk Tactics", font = "digital") 
+        print(logo, welcome, "\nby Rachael Babister\n")
+        print("\nGame Loading...\n")
+        time.sleep(2)
+
+        # ENTER NAME AND START GAME
+        """
+        User enters their name in order for the game rules to appear.
+        Once validated, the user can then specify y/n to play the game.
+        """
+        while True:
+            self.user_name = input("Enter your name: ")
+            if self.user_name == "":
+                print("You need to enter your name in order to play")
+            else:
+                break
+
+        clear()
+        print(pyfiglet.figlet_format("Welcome to Tomahawk Tactics", font = "digital"))
+        print()
+        print(f"Hello {self.user_name}! \n")
+        print("""
 This is Tomahawk Tactics, an axe throwing game in which you need
 to hit a moving target! You get 5 throws per game, and will play
 against the computer who will also get 5 throws. The winner is the
 player who hits 3 targets first. To select a target, imagine there
 are rows (in letters) and columns (in numbers), so an example of 
 your guess might look like 'C3'.
-    """)
+        """)
 
-    # READY TO PLAY FUNCTION
-    def ready_to_play():
-        """
-        User inputs whether they are ready to play or not. 
-        If 'y' the game creates the target ready to play. 
-        If 'n', the program stops.
-        """
-        while True:
-            user_input = input("Are you ready to play? (y/n): ").lower()
-            if user_input == 'y':
-                print("\nGreat! Get your axe ready!")
-                return True
-            elif user_input == 'n':
-                print("\nAxes down, see you next time. Goodbye.")
-                return False
-            else:
-                print("Invalid input. Please enter 'y' or 'n'.")
-    return ready_to_play()              
+        # READY TO PLAY FUNCTION
+        def ready_to_play():
+            """
+            User inputs whether they are ready to play or not. 
+            If 'y' the game creates the target ready to play. 
+            If 'n', the program stops.
+            """
+            while True:
+                user_input = input("Are you ready to play? (y/n): ").lower()
+                if user_input == 'y':
+                    print("\nGreat! Get your axe ready!")
+                    return True
+                elif user_input == 'n':
+                    print("\nAxes down, see you next time. Goodbye.")
+                    return False
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'.")
+        return ready_to_play()
 
 
 # CALLING ALL FUNCTIONS
@@ -149,7 +149,8 @@ def main():
     """
     Runs all funcions
     """
-    user_name = game_logo()
+    play = TomaHawkGame()
+    play.run_game()
     target_boards = TargetBoards()
     target_boards.display_target()
 
